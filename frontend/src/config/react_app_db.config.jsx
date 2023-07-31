2ab0f6363ed08d2993503f687fa55e8b1887d491
victhangnguyen <victhangnguyen@outlook.com>
2023-08-01 04:40:14 +0700

    Change config to Deploy

--- frontend/src/config/react_app_db.config.jsx
+++ frontend/src/config/react_app_db.config.jsx
@@ -1,12 +1,12 @@
 const REACT_APP_DB_PORT = 5000;
 const REACT_APP_DB_HOST = "http://127.0.0.1";
-// const REACT_APP_DB_URL = "https://foxv-ecommerce-beta.onrender.com";
-const REACT_APP_DB_URL = "";
+const REACT_APP_DB_URL = "https://foxv-ecommerce-beta.onrender.com";
+// const REACT_APP_DB_URL = "";
 
 const REACT_APP_PORT = 3000;
 const REACT_APP_HOST = "http://127.0.0.1";
-// const REACT_APP_URL = "https://foxv.onrender.com";
-const REACT_APP_URL = "";
+const REACT_APP_URL = "https://foxv.onrender.com";
+// const REACT_APP_URL = "";
 
 const react_app_db = {
   reactAppURL: REACT_APP_URL || `${REACT_APP_HOST}:${REACT_APP_PORT}`,
